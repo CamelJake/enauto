31e19b17d10602895349b99b0a45e9bfeb8b99ee
Jacob Bennett <xxencryptedsoldierxx@gmail.com>
2023-07-01 14:17:34 -0400

    Modified the netmiko lab to use yaml for host file and Jinja 2 template to push commands

--- Python/netmikolab.py
+++ Python/netmikolab.py
@@ -1,21 +1,26 @@
 from netmiko import Netmiko
 from yaml import safe_load
+from jinja2 import Environment, FileSystemLoader
 
 def main():
-    device = {
-        "host":"sandbox-iosxe-latest-1.cisco.com",
-        "username":"admin",
-        "password" : "C1sco12345",
-        "device_type" : "cisco_ios",
-    }
-    net_conn = Netmiko(**device)
-    config = ["interface loopback69", "desc Test loopback"]
-    net_conn.send_config_set(config)
-    output = net_conn.send_command("show run int loopback69")
-    net_conn.save_config()
-
-
-    print(output)
+    #Define Jinja 2 environment. Load templates from my C: drive
+    environment = Environment(loader=FileSystemLoader("C:/Users/xxenc/OneDrive/Documents/enauto_coding/venv/enauto/Python/Templates/"))
+    #Load in the commands template from the template folder
+    template = environment.get_template("commands.txt")
+    #Open the Yaml file which contains hosts
+    with open(r'C:\Users\xxenc\OneDrive\Documents\enauto_coding\venv\enauto\Python\device.yml', 'r') as file:
+        devices = safe_load(file)
+    for device in devices["hosts"]:
+        net_conn = Netmiko(
+            host=device["host"],
+            username=device["username"],
+            password=device["password"],
+            device_type=device["device_type"],
+        )
+        #Render the tempalte based on device type. The type of command run depends on the device type.
+        commands = template.render(device_type = device["device_type"])
+        output = net_conn.send_command(commands)
+        print(output)
 if __name__ == "__main__":
     main()
 
